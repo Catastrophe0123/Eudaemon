6ad632a28e4102d6571eb22b2872116e8c93b003
Catastrophe <vnavaneeth15@gmail.com>
2020-07-01 10:45:12 +0530

    reset-starting over

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    JWTSECRET: 'sih winner pm dinner',
-    MONGOURI: 'mongodb://127.0.0.1:27017/eudaemon'
-};
--- models/Child.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const childSchema = new mongoose.Schema({
-    name: String,
-    age: String,
-    guardian: { type: mongoose.Schema.Types.ObjectId, ref: 'Guardian' }
-});
-
-module.exports = mongoose.model('Child', childSchema);
